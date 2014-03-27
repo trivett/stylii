a2912d8aff77fdd2d40e07a808bcb650559b4f70
trivett <trivett@gmail.com>
2014-03-27 17:16:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stylii::Application.config.secret_key_base = '69c99546d6f27631e04185c9c741e3232b980e21960af20019ef8e9ee4dfc587471a892f2f2c9f839e6eb856a7f72cee02c379a3e0020dd297788183c9893ba2'
